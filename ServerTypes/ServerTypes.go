eae1706fe7bc766db095873db1abca14552a18a6
shane mendez <mendezsoftware@gmail.com>
2020-10-21 15:55:36 -0500

    working on config and mux as handler

---
+++ ServerTypes/ServerTypes.go
@@ -0,0 +1,5 @@
+package ServerTypes
+
+const (
+	ReverseProxy = "ReverseProxy"
+)
--- proxy.go
+++ proxy.go
@@ -1,26 +1,69 @@
 package main
 
 import (
+	// "os"
 	"log"
-	"net/http"
-	"net/http/httputil"
 	"net/url"
+	"net/http"
+	// "encoding/json"
+	"net/http/httputil"
+	// "./ServerTypes"
 )
 
-func main(){
-	origin, _ := url.Parse("http://localhost:3000")
-	director := func(req *http.Request){
-		// log.Printf("%s",req.Header.Values("X-Forwarded-For"))
-		req.Header.Del("X-Forwarded-For")
-		req.Header.Add("X-Forwarded-Host", req.Host)
-		req.Header.Add("X-Origin-Host", origin.Host)
-		req.URL.Scheme = "http"
-		req.URL.Host = origin.Host
-	}
-	proxy := &httputil.ReverseProxy{Director: director}
+type Handler = func(res http.ResponseWriter, req *http.Request)
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-		proxy.ServeHTTP(w,r)
+
+type Service struct{
+	Host string
+	Port int
+	Handlers []Handler
+}
+
+type Config struct {
+
+}
+
+type MuxConfig struct {
+	Config
+	Hostname string
+	Port int
+	ServerType string
+}
+
+
+
+func parseConfig(config string) map[string]Service{
+	origin , _ := url.Parse("http://shanemendez.com")
+	port := 3000
+
+	
+	return map[string]Service{
+		origin.Host : Service{Host: origin.Host, Port: port},
+	}
+
+
+}
+
+func buildMux(config MuxConfig) http.ServeMux{
+	mux := http.NewServeMux()
+	remote, _ := url.Parse("http://localhost:3000")
+	proxy := httputil.NewSingleHostReverseProxy(remote)
+
+
+	http.NewServeMux()
+	mux.HandleFunc("shanemendez.com/", func(res http.ResponseWriter, req *http.Request){
+		proxy.ServeHTTP(res,req)
 	})
-	log.Fatal(http.ListenAndServe(":2999", nil))
+	return *mux
+}
+
+func main(){
+
+	// config, _ := os.Open("config.json")
+	
+	
+
+	mux := buildMux(MuxConfig{})
+	log.Fatal(http.ListenAndServe(":9000", mux.ServeHTTP()))
+	
 }
